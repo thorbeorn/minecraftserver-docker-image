0a9f9592589db3108084f1f7257c9e26cd3b64f2
Dylan LLODRA <dylanllodrapro@gmail.com>
2023-02-19 22:20:40 +0100

    modif dashboard

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,8 @@
         <div class="app">
             <div class="dashboard-indicateur">
                 <div class="indicateur-item">
-                    <div class="indicateur-gauge"><div class="indicateur-gauge-back"><div class="indicateur-gauge-front red"><div><img class="fit-picture" src="cpu.png" alt="CPU"><p>50 %</p></div><svg><circle style="stroke-dasharray:calc(var(--dasharray-max) * 50 / 100)" class="red" fill="white" r="30%" cx="50%" cy="50%"/></svg></div></div><p class="indicateur-gauge-label">Consomation CPU</p></div>
+                    <div class="indicateur-gauge"><div class="indicateur-gauge-back"><div class="indicateur-gauge-front red"><div><img class="fit-picture" src="cpu.png" alt="CPU"><p class="pourcent">20 %</p></div><svg><circle style="stroke-dasharray:calc(var(--dasharray-max) * 20 / 100), var(--dasharray-max);" class="red" fill="white" r="30%" cx="50%" cy="50%"/></svg></div></div><p class="indicateur-gauge-label">Consomation CPU</p></div>
+                    <div class="indicateur-gauge"><div class="indicateur-gauge-back"><div class="indicateur-gauge-front blue"><div><img class="fit-picture" src="cpu.png" alt="CPU"><p class="pourcent">40 %</p></div><svg><circle style="stroke-dasharray:calc(var(--dasharray-max) * 40 / 100), var(--dasharray-max);" class="blue" fill="white" r="30%" cx="50%" cy="50%"/></svg></div></div><p class="indicateur-gauge-label">Consomation RAM</p><p class="indicateur-gauge-label2">3,6 Go / 16 Go</p></div>
                 </div>
             </div>
         </div>
