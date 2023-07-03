6eedfb4610d4c36810ff7752a3a626a132a6f9ec
Foufou-exe <thibaut.maurras34@gmail.com>
2023-07-03 20:05:49 +0200

    🐞 fix(serveur.html):
    Ajout et modification des animation js et mode Dark/light

--- app/public/src/main.js
+++ app/public/src/main.js
@@ -9,23 +9,57 @@ tippy('[data-tippy-content]');
 // Toggle sidebar
 $(document).ready(function() {
     $("#aside-toggle").click(function() {
-        $("#logo-sidebar").toggle();
+        let logoSidebar = $("#logo-sidebar");
+        if (logoSidebar.is(":visible")) {
+            logoSidebar.animate({width: 'toggle', opacity: 0}, 500);
+        } else {
+            logoSidebar.animate({width: 'toggle', opacity: 1}, 500);
+        }
     });
 });
 
-// Mode Thèmes (Dark/Light)
-const modeToggle = document.getElementById('modeToggle');
-const icon = document.getElementById('icon');
 
-modeToggle.addEventListener('click', function() {
-  if (icon.classList.contains('fa-sun')) {
-    icon.classList.remove('fa-sun');
-    icon.classList.add('fa-moon');
-  } else {
-    icon.classList.remove('fa-moon');
-    icon.classList.add('fa-sun');
-  }
+// Mode Thèmes (Dark/Light)
+window.onload = () => {
+    console.log("Page loaded");
+    // Load theme state from localStorage
+    if (localStorage.getItem('darkMode') === 'true') {
+        console.log("Dark mode detected in localStorage");
+        toggleDarkMode();
+    }
+};
+
+const toggleDarkMode = () => {
+    const logo = document.getElementById('logo');
+    const icon = document.getElementById('icon');
+
+    console.log("Toggling dark mode");
+    
+    if (document.documentElement.classList.toggle('dark')) {
+        console.log("Switching to dark mode");
+        // Change logo and icon for dark mode
+        logo.src = "public/assets/img/logo_solo.png";
+        icon.classList.replace("fa-moon", "fa-sun");
+
+        // Save theme state to localStorage
+        localStorage.setItem('darkMode', 'true');
+
+    } else {
+        console.log("Switching to light mode");
+        // Change logo and icon back for light mode
+        logo.src = "public/assets/img/logo_soloblack.png";
+        icon.classList.replace("fa-sun", "fa-moon");
+
+        // Save theme state to localStorage
+        localStorage.setItem('darkMode', 'false');
+    }
+};
+
+document.getElementById('modeToggle').addEventListener('click', () => {
+    console.log("Mode toggle clicked");
+    toggleDarkMode();
 });
+
 
 
 
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // or 'media' or 'class'
   content: ['./public/assets/js/*.js', './public/assets/css/*.css','*.html','./views/*.php','./views/permanents/*.php'],
   theme: {
     extend: {},
